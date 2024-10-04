758a9c0e88ac05a26309ae1a00ef234b79132d43
votantai0909 <tantaivo0909@gmail.com>
2024-10-04 08:45:49 +0700

    .

--- HairSalonBookingApp/HairSalon.Repositories/Data/Migrations/20241003015905_First.Designer.cs
+++ HairSalonBookingApp/HairSalon.Repositories/Data/Migrations/20241003015905_First.Designer.cs
@@ -4,16 +4,19 @@ using HairSalon.Repositories.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace HairSalon.Repositories.Migrations
+namespace HairSalon.Repositories.Data.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20241003015905_First")]
+    partial class First
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
